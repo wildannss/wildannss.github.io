ce790e4d93011cefded6a31c4a8334d9ec3059aa
wildan <wildan.syarif1021@gmail.com>
2020-12-14 09:41:59 +0700

    col

--- mine/colab/gpu/mine.js
+++ mine/colab/gpu/mine.js
@@ -33,14 +33,14 @@ for(a=1; a<=30; a++){
         if(error1.test(run)){
             iimSet("loop",i);
             iimPlay("colab/gpu/logout");
-            iimSet("loop",i);
-            iimPlay("colab/gpu/traffic");
+            // iimSet("loop",i);
+            // iimPlay("colab/gpu/traffic");
             i--;
         }else if(error12.test(run)){
             iimSet("loop",i);
             iimPlay("colab/gpu/logout_f");
-            iimSet("loop",i);
-            iimPlay("colab/gpu/traffic");
+            // iimSet("loop",i);
+            // iimPlay("colab/gpu/traffic");
             i--;
 
         //JIKA TIDAK ADA EMAIL DI FORM BARU
@@ -52,8 +52,8 @@ for(a=1; a<=30; a++){
             if(error3.test(run2)){
                 iimSet("loop",i);
                 iimPlay("colab/gpu/logout");
-                iimSet("loop",i);
-                iimPlay("colab/gpu/traffic");
+                // iimSet("loop",i);
+                // iimPlay("colab/gpu/traffic");
             //FORM LAMA
             }else if(error9.test(run2)){
                 iimSet("loop",i);
@@ -69,14 +69,14 @@ for(a=1; a<=30; a++){
                         if(error3.test(run4)){
                             iimSet("loop",i);
                             iimPlay("colab/gpu/logout");
-                            iimSet("loop",i);
-                            iimPlay("colab/gpu/traffic");
+                            // iimSet("loop",i);
+                            // iimPlay("colab/gpu/traffic");
                         }
-                        iimSet("loop",i);
-                        iimPlay("colab/gpu/traffic");
+                        // iimSet("loop",i);
+                        // iimPlay("colab/gpu/traffic");
                     }
-                    iimSet("loop",i);
-                    iimPlay("colab/gpu/traffic");
+                    // iimSet("loop",i);
+                    // iimPlay("colab/gpu/traffic");
                 }else if(error4.test(run3)){
                     //JIKA ADA CAPTCHA
                     iimSet("loop",i);
@@ -85,15 +85,15 @@ for(a=1; a<=30; a++){
                     if(error3.test(run4)){
                         iimSet("loop",i);
                         iimPlay("colab/gpu/logout");
-                        iimSet("loop",i);
-                        iimPlay("colab/gpu/traffic");
+                        // iimSet("loop",i);
+                        // iimPlay("colab/gpu/traffic");
                     }
                 //JIKA SUDAH DIPAKAI
                 }else if(error3.test(run3)){
                     iimSet("loop",i);
                     iimPlay("colab/gpu/logout");
-                    iimSet("loop",i);
-                    iimPlay("colab/gpu/traffic");
+                    // iimSet("loop",i);
+                    // iimPlay("colab/gpu/traffic");
                 }
             }
 
@@ -101,29 +101,29 @@ for(a=1; a<=30; a++){
         }else if(error3.test(run)){
             iimSet("loop",i);
             iimPlay("colab/gpu/logout");
-            iimSet("loop",i);
-            iimPlay("colab/gpu/traffic");
+            // iimSet("loop",i);
+            // iimPlay("colab/gpu/traffic");
         
         //JIKA ADA CAPTCHA
         }else if(error10.test(run)){
             iimSet("loop",i);
             iimPlay("colab/gpu/login_cap");
-            iimSet("loop",i);
-            iimPlay("colab/gpu/traffic");
+            // iimSet("loop",i);
+            // iimPlay("colab/gpu/traffic");
         
         //JIKA NOT RESPONDING
         }else if(error5.test(run)){
             iimSet("loop",i);
             iimPlay("colab/gpu/logout_f");
-            iimSet("loop",i);
-            iimPlay("colab/gpu/traffic");
+            // iimSet("loop",i);
+            // iimPlay("colab/gpu/traffic");
         
         //JIKA ELEMEN BUTTON TIDAK DITEMUKAN
         }else if(error13.test(run)){
             iimSet("loop",i);
             iimPlay("colab/gpu/logout_f");
-            iimSet("loop",i);
-            iimPlay("colab/gpu/traffic");
+            // iimSet("loop",i);
+            // iimPlay("colab/gpu/traffic");
         
         }
     }
